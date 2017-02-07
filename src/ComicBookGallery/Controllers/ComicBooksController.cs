f42fc73ba7ea8c82f45da9893b4208a5d5b75a85
Akram Faheem ayoub Shenoda <akramShenoda@outlook.com>
2017-02-07 20:14:01 +0400

    Added detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace ComicBookGallery.Controllers
 {
-    class ComicBooksController : System.Web.Mvc.Controller
+   public  class ComicBooksController : System.Web.Mvc.Controller
     {
+        
+        public string detail()
+        {
 
+            return "HELLO FORM THE COMIC BOOKS CONTROLLER!";
+
+        }
 
     }
 }
